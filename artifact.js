99efdedd711b676a5c910b78568794162a8dcecc
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2024-06-06 03:23:36 +0930

    test

--- artifact.js
+++ artifact.js
@@ -8,6 +8,6 @@ var title = document.getElementById("title")
 
 title.innerHTML = data.itemLabel
 
-var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {models: [{id:0, filename: `https://cors-anywhere.herokuapp.com/${data.model}?origin=*`}]}); 
+var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {models: [{id:0, filename: `${data.model}?origin=*`}]}); 
 // = data.itemLabel
 //Here you may write your code, after this document.load will fire
