cfcd6649815e9aa13f8f14867dcea7f189508ab6
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2024-06-06 15:01:26 +0930

    Agh

--- artifact.js
+++ artifact.js
@@ -4,10 +4,10 @@ data = JSON.parse(decodeURIComponent(data))
 console.log(data)
 
 
-var title = document.getElementById("title")
-var description = document.getElementById("description")
-description.innerHTML = data.itemDescription
-title.innerHTML = data.itemLabel
+//var title = document.getElementById("title")
+//var description = document.getElementById("description")
+//description.innerHTML = data.itemDescription
+//title.innerHTML = data.itemLabel
 
 var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {models: [{id:0, filename: `${data.model}?origin=*`}]}); 
 // = data.itemLabel
