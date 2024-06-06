0525830e0269776d32bdf36f980ee306e5fa5483
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2024-06-06 16:43:58 +0930

    thknas skid

--- artifact.js
+++ artifact.js
@@ -8,7 +8,22 @@ console.log(data)
 //var description = document.getElementById("description")
 //description.innerHTML = data.itemDescription
 //title.innerHTML = data.itemLabel
-
+var img = document.getElementById("image")
+img.src = data.pic
 var stl_viewer = new StlViewer(document.getElementById("stl_cont"), {models: [{id:0, filename: `${data.model}?origin=*`}]}); 
 // = data.itemLabel
+//${data.model}?origin=*
 //Here you may write your code, after this document.load will fire
+
+function switchImage() {
+	var oldIndex = 0;
+	var images = document.getElementById("images")
+	for (i = 0; i < images.children.length; i++) {
+		if (images.children[i].style.display == "block") {
+			oldIndex = i
+		}
+		images.children[i].style.display = "none"
+	}
+
+	images.children[(oldIndex + 1) % images.children.length].style.display = "block" 
+}
