3235ffc197f3065a770bbe15d21dbf750f106941
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2024-06-06 13:56:09 +0930

    pages

--- src.js
+++ src.js
@@ -4,11 +4,47 @@ async function loadJSON(path) {
     return json
 }
 
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
+
 async function loadObjects(path) {
     var objs = await loadJSON(path)
     var table = document.getElementById("results")
 
-    for (i in objs) {
+    var queryString = window.location.search.substring(1);
+    queryString = parseQuery(queryString)
+    var page = Number(queryString.page)
+
+    if (isNaN(page)) {
+        page = 0
+    }
+
+    console.log(page)
+
+    var lower = 10 * page
+    var upper = lower + 10 < objs.length ? lower + 10 : objs.length
+
+    if (page != 0) {
+        var prev = document.getElementById("prev")
+        prev.href = `artifacts.html?page=${page - 1}`
+    } else {
+        ;        
+    }
+
+    if (page * 10 + 10 < objs.length) {
+        var prev = document.getElementById("next")
+        prev.href = `artifacts.html?page=${page + 1}`
+    }
+
+
+    for (var i = lower; i < upper; i++) {
         var obj = objs[i]
         var link = `artifact.html?${encodeURIComponent(JSON.stringify(obj))}`
 
