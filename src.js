2e92dea9614b0d4ee96499135f81007e45f6e5ff
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2024-06-06 13:30:40 +0930

    pics

--- src.js
+++ src.js
@@ -15,7 +15,7 @@ async function loadObjects(path) {
         var row = ""
         row += `<tr style="display: table-row;">`
         row += `<td><a class="table-entry" href="${link}">${i}</a></td>`
-        row += `<td><a class="table-entry" href="${link}"><img src="assets/skull.jpg" style="max-height: 64px; max-width: 128px;"></a></td>`
+        row += `<td><a class="table-entry" href="${link}"><img src="${obj.thumb}" style="max-height: 64px; max-width: 128px;"></a></td>`
         row += `<td style="text-align: left;"><a class="table-entry" href="${link}">${obj.itemLabel}<br>${obj.itemDescription}</a></td>`
         row += `<td><a class="table-entry" href="${link}">Yes</a></td>`
         row += `<td><a class="table-entry" href="${link}">No</a></td>`
